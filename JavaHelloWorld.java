572f363299fc040210f3e75b0e85b6311e327e5a
zhangxpp <22886149@qq.com>
2016-03-23 15:34:22 +0800

    Added files via upload

---
+++ JavaHelloWorld.java
@@ -0,0 +1,9 @@
+public class JavaHelloWorld
+{
+    public static void main(String [] args)
+    {
+         System.out.println("Java Hello World");
+	 System.out.println("Hello again");
+	 System.out.println("Add three comments");
+    }
+}
